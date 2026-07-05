3aa8f88484d05e58a561b58009f0465af0c24003
Derell Licht <derelict@comcast.net>
2026-07-05 16:35:19 -0700

    Makefile: change d:/clang to d:/llvm

--- version.h
+++ version.h
@@ -4,6 +4,7 @@
 //  version    changes
 //   =======   ======================================
 //    1.00     Initial release
+//    1.01     Changed hyperlink color from fixed color to COLOR_HOTLIGHT
 //*********************************************************************************************
-#define VerNum    "V1.00"
+#define VerNum    "V1.01"
 
